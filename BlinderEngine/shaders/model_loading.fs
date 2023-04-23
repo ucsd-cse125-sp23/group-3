8c9ac9e03c6afc13294ac8f830cf51d6556928bc
Cryszzz <69176305+Cryszzz@users.noreply.github.com>
2023-04-23 13:27:46 -0700

    edit shader lighting

--- BlinderEngine/shaders/model_loading.fs
+++ BlinderEngine/shaders/model_loading.fs
@@ -2,10 +2,20 @@
 out vec4 FragColor;
 
 in vec2 TexCoords;
+in vec3 fragNormal;
 
+uniform vec3 AmbientColor = vec3(0.2);
+uniform vec3 LightDirection = normalize(vec3(1, 5, 2));
+uniform vec3 LightColor = vec3(0.5);
 uniform sampler2D texture_diffuse1;
 
 void main()
 {    
-    FragColor = texture(texture_diffuse1, TexCoords);
+    // Compute irradiance (sum of ambient & direct lighting)
+    vec3 irradiance = AmbientColor + LightColor * max(0, dot(LightDirection, fragNormal));
+    
+    vec3 DiffuseColor = vec3(texture(texture_diffuse1, TexCoords));
+    vec3 reflectance = irradiance * DiffuseColor;
+    FragColor = vec4(sqrt(reflectance), 1);
+    //FragColor=texture(texture_diffuse1, TexCoords);
 }
--- BlinderEngine/shaders/model_loading.vs
+++ BlinderEngine/shaders/model_loading.vs
@@ -4,6 +4,7 @@ layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
 
 out vec2 TexCoords;
+out vec3 fragNormal;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -13,4 +14,5 @@ void main()
 {
     TexCoords = aTexCoords;    
     gl_Position = projection * view * model * vec4(aPos, 1.0);
+    fragNormal = vec3(model * vec4(aNormal, 0));
 }
