4991d19be80d9dbde914c2f84b2f39e44f40c9ef
Yucheng Huang <yuh032@UCSD.EDU>
2023-04-27 12:00:39 -0700

    Add: added solod lib

--- BlinderEngine/src/Window.cpp
+++ BlinderEngine/src/Window.cpp
@@ -74,7 +74,7 @@ bool Window::initializeObjects(int PlayID) {
         players.at(i)=temp;
     }
     playerID = PlayID;
-    daeObject1 = new DaeObject("./resources/objects/girl/girl.dae", glm::vec3(5.0f, 5.0f, 5.0f));
+    daeObject1 = new DaeObject("./resources/objects/girl/girl.dae", glm::vec3(4.0f));
     objObject1 = new ObjObject("./resources/objects/ucsd_asset/bear.obj", glm::vec3(0.4f, 0.4f, 0.4f));
 
     daeObjectList.push_back(daeObject1);
@@ -164,7 +164,7 @@ void Window::idleCallback() {
     
     if (playerID == 0) {
 
-        Cam->setFirstperson();
+        //Cam->setFirstperson();
     }
     Cam->SetModel(players.at(playerID)->getModel());
     Cam->Update();
