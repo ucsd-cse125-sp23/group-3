448fb36a638b1de50667c9634a9b58d8e15a44bd
yingkailee <yil179@ucsd.edu>
2023-05-03 15:50:21 -0700

    pSrc

--- server/include/ObstaclesSv.h
+++ server/include/ObstaclesSv.h
@@ -15,4 +15,6 @@ public:
 	// indexed by id
 	std::vector<ObsObjectSv*> obs_vec;
 	std::vector<glm::mat4> glm_vec;
+
+	void pairUp() { for (int ind = 0; ind < obs_vec.size(); ind++) obs_vec[ind]->pSrc = glm_vec[ind]; };
 };
