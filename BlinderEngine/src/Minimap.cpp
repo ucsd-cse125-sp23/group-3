b339d9b90145ec5165bb2c38a0df17572f56bc0a
Cryszzz <69176305+Cryszzz@users.noreply.github.com>
2023-05-15 16:59:02 -0700

    change for minimap

--- BlinderEngine/src/Minimap.cpp
+++ BlinderEngine/src/Minimap.cpp
@@ -13,7 +13,10 @@ Minimap::Minimap(float _sizeX,float _sizeY, float _positionX, float _positionY){
     positionY=_positionY;
     person=new graphic2D( Constants::MINIMAP_OBJECT_SIZE*sizeX, Constants::MINIMAP_OBJECT_SIZE*_sizeY,  _positionX+0.5*sizeX-Constants::MINIMAP_OBJECT_SIZE/2*sizeX,  _positionY+0.5*sizeY-Constants::MINIMAP_OBJECT_SIZE/2*sizeY,true);
     const char* persontextfile="./images/person.png";
+    person2=new graphic2D( Constants::MINIMAP_OBJECT_SIZE*sizeX, Constants::MINIMAP_OBJECT_SIZE*_sizeY,  _positionX+0.5*sizeX-Constants::MINIMAP_OBJECT_SIZE/2*sizeX,  _positionY+0.5*sizeY-Constants::MINIMAP_OBJECT_SIZE/2*sizeY,true);
+    const char* persontextfile="./images/person_Alice.png";
     person->bindTexture(persontextfile);
+    Alice_shown=false;
 }
 
 Minimap::~Minimap(){
@@ -23,11 +26,15 @@ Minimap::~Minimap(){
 void Minimap::draw(const glm::mat4& viewProjMtx, StaticShader shader){
     minimap->draw(shader, 1.0f);
     person->draw(shader, 1.0f);
+    if(Alice_shown){
+        person2->draw(shader, 1.0f);
+    }
 }
 
 void Minimap::update(){
     minimap->update();
     person->update();
+    person2->update();
 }
 
 void Minimap::setPosition(glm::mat4 model){
@@ -35,6 +42,13 @@ void Minimap::setPosition(glm::mat4 model){
     personx=(pSource[12]+GROUND_SIZE)/(GROUND_SIZE*2)*sizeX;
     persony=(-pSource[14]+GROUND_SIZE)/(GROUND_SIZE*2)*sizeY;
     person->setposition( Constants::MINIMAP_OBJECT_SIZE*sizeX, Constants::MINIMAP_OBJECT_SIZE*sizeY,  positionX+personx-Constants::MINIMAP_OBJECT_SIZE/2*sizeX,  positionY+persony-Constants::MINIMAP_OBJECT_SIZE/2*sizeY);
+}
+
+void Minimap::setPositionAlice(glm::mat4 model){
+    float *pSource = (float*)glm::value_ptr(model);
+    personx2=(pSource[12]+GROUND_SIZE)/(GROUND_SIZE*2)*sizeX;
+    persony2=(-pSource[14]+GROUND_SIZE)/(GROUND_SIZE*2)*sizeY;
+    person2->setposition( Constants::MINIMAP_OBJECT_SIZE*sizeX, Constants::MINIMAP_OBJECT_SIZE*sizeY,  positionX+personx2-Constants::MINIMAP_OBJECT_SIZE/2*sizeX,  positionY+persony2-Constants::MINIMAP_OBJECT_SIZE/2*sizeY);
 }
 
 graphic2D* Minimap::getMinimap() {
