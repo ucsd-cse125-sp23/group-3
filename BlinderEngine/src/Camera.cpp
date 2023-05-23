d67b5f44acbb0034f00ab3c489ca968a4f948210
Cryszzz <69176305+Cryszzz@users.noreply.github.com>
2023-05-23 11:57:51 -0700

    change for walls

--- BlinderEngine/src/Camera.cpp
+++ BlinderEngine/src/Camera.cpp
@@ -15,7 +15,7 @@ void Camera::Update() {
     // Compute camera world matrix
 
     if (first_person) {
-        CameraTar = glm::vec3(0.0f, 3.0f, 0.0f);
+        CameraTar = glm::vec3(0.0f, 4.0f, 0.0f);
         CameraDir = glm::vec3(0.0f, 0.0f, 1.0f);
         CameraPos = CameraTar + CameraDir;
     }
