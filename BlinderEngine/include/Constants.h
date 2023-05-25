5a99a404af2d6511a183254b7b649c5e4a59bff9
Yucheng Huang <yuh032@UCSD.EDU>
2023-05-25 12:18:44 -0700

    clean up resources and constants

--- BlinderEngine/include/Constants.h
+++ BlinderEngine/include/Constants.h
@@ -7,6 +7,7 @@
 
 namespace Constants
 {
+	// Shader path
 	const std::string static_shader_vert = "./shaders/model_loading.vs";
 	const std::string static_shader_frag = "./shaders/model_loading.fs";
 	
@@ -15,21 +16,6 @@ namespace Constants
 
 	const std::string dynamic_shader_vert = "./shaders/anim_model.vs";
 	const std::string dynamic_shader_frag = "./shaders/anim_model.fs";
-
-	// Define object path
-	const std::string nanosuit_object_path = "./resources/objects/nanosuit/nanosuit.obj";
-	const std::string backpack_object_path = "./resources/objects/backpack/backpack.obj";
-	const std::string rock_object_path = "./resources/objects/rock/rock.obj";
-	const std::string drone_object_path = "./resources/objects/uav/uav.dae";
-
-	const std::string drone_object_animation = "./resources/objects/uav/uav.dae";
-
-	// Define scaling factor for objects
-	const glm::vec3 nanosuit_scaling_factor = glm::vec3(0.5f, 0.5f, 0.5f);
-	const glm::vec3 backpack_scaling_factor = glm::vec3(2.0f, 2.0f, 2.0f);
-	const glm::vec3 rock_scaling_factor = glm::vec3(5.0f, 5.0f, 5.0f);
-	const glm::vec3 drone_scaling_factor = glm::vec3(1.0f, 1.0f, 1.0f);
-
 
 	// Alice model & anmiation
 	const std::string alice_model_path = "./resources/objects/alice/alice.dae";
@@ -66,11 +52,10 @@ namespace Constants
 	const std::string exitdoor_model_path = "./resources/objects/exit/Exitdoor_tri.obj";
 	const glm::vec3 exitdoor_scaling_factor = glm::vec3(1.0f);
 
+
+	// Env
 	const bool offline = true;
-
-
 	const int DIST_LIMIT = 20;
-
 	const float MINIMAP_OBJECT_SIZE=0.02f;
 
 };
