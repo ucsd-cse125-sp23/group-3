5f0988febe7ed059de78a53f0423a43f97380072
Antonia Manshi Yang <may003@ucsd.edu>
2023-05-25 12:05:38 -0700

    Add seerver outer loop&initialization

--- server/main.cpp
+++ server/main.cpp
@@ -4,8 +4,7 @@
 int main()
 {
      Server* serv = new Server();
-     //for (auto a : serv->ids)
- 	 //   std::cout << a << " ";
+
      // blocks until 4 clients
      for (int id = 0; id < NUM_PLAYERS; id ++){
          SOCKET ss = INVALID_SOCKET;
@@ -16,80 +15,87 @@ int main()
          serv->send_init_packet(id, id);
          std::cout << "connected ss " << ss <<" with id" << serv->ids[id] << std::endl;
      }
-
-     // send initial gamedata to all clients
-     for (int i = 0; i < NUM_PLAYERS; i++)
-     {
-         serv->send_gamedata(i);
-     }
-
-     // character selection
-     int num_selection = 0;
-     int idx = 0;
-     while (num_selection < 4)
-     {
-         int character = serv->handle_acq(idx % NUM_PLAYERS);
-         if (character != -1) num_selection++;
-         idx++;
-
-         //if (idx % 500000 == 0)
-         //std::cout << serv->ids[0] << " " << serv->ids[1] << " " << serv->ids[2] << " " << serv->ids[3] << std::endl;
-     }
-
-     // wait for ready action and send init packet;blocks until 4 connect
-     for (int i = 0; i < NUM_PLAYERS; i++)
-     {
-         int check_recv_ready = serv->recv_event(i);
-         while (check_recv_ready != (int)EventType::READY) {
-             check_recv_ready = serv->recv_event(i);
-         }
-         // serv->send_init_packet(i, serv->ids[i]); // TODO: add randomly assign character logic
-     }
-
-     SOCKET cp_sessions[NUM_PLAYERS];
-
-     // Reorder sessions with: A,B,C,D
-     //for (int ind = 0; ind < 4; ind ++) {
-     //    cp_sessions[ind] = serv->sessions[serv->ids[ind]];
-     //}
-     for (int i = 0; i < 4; i++)
-     {
-         auto it = std::find(serv->ids.begin(), serv->ids.end(), i);
-         int client_id_idx = it - serv->ids.begin();
-         cp_sessions[i] = serv->sessions[client_id_idx];
-     }
-     for (int ind = 0; ind < 4; ind++) {
-         serv->sessions[ind] = cp_sessions[ind];
-     }
-
-     // send updated game data
-     serv->gd->gamestate = GameState::IN_GAME;
-     for (int i = 0; i < NUM_PLAYERS; i++)
-     {
-         serv->send_gamedata(i);
-     }
-
      while (1) {
+         // reinitialize all needed data
+         serv->initialize_game();
 
-         std::chrono::milliseconds start = std::chrono::duration_cast<std::chrono::milliseconds>(
-             std::chrono::system_clock::now().time_since_epoch()
-         );
+         // send initial gamedata to all clients
+         for (int i = 0; i < NUM_PLAYERS; i++)
+         {
+             serv->send_gamedata(i);
+         }
 
-         std::chrono::milliseconds end = std::chrono::duration_cast<std::chrono::milliseconds>(
-             std::chrono::system_clock::now().time_since_epoch()
-         );
+         // character selection
+         int num_selection = 0;
+         int idx = 0;
+         while (num_selection < 4)
+         {
+             int character = serv->handle_acq(idx % NUM_PLAYERS);
+             if (character != -1) num_selection++;
+             idx++;
+         }
 
-         serv->check_event = {-1, -1, -1, -1};
-         std::unordered_map<int, std::vector<int>> all_records;
-         while (end - start < (std::chrono::milliseconds)LISTEN_TICK) {
+         // wait for ready action and send init packet;blocks until 4 connect
+         for (int i = 0; i < NUM_PLAYERS; i++)
+         {
+             int check_recv_ready = serv->recv_event(i);
+             while (check_recv_ready != (int)EventType::READY) {
+                 check_recv_ready = serv->recv_event(i);
+             }
+         }
 
-             for (int i = 0; i < NUM_PLAYERS; i++)
-             {
-                 if (serv->check_event[i] != -1)
+         SOCKET cp_sessions[NUM_PLAYERS];
+
+         // Reorder sessions with: A,B,C,D
+         for (int i = 0; i < 4; i++)
+         {
+             auto it = std::find(serv->ids.begin(), serv->ids.end(), i);
+             int client_id_idx = it - serv->ids.begin();
+             cp_sessions[i] = serv->sessions[client_id_idx];
+         }
+         for (int ind = 0; ind < 4; ind++) {
+             serv->sessions[ind] = cp_sessions[ind];
+         }
+
+         // send updated game data
+         serv->gd->gamestate = GameState::IN_GAME;
+         for (int i = 0; i < NUM_PLAYERS; i++)
+         {
+             serv->send_gamedata(i);
+         }
+
+         while (1) {
+
+             std::chrono::milliseconds start = std::chrono::duration_cast<std::chrono::milliseconds>(
+                 std::chrono::system_clock::now().time_since_epoch()
+             );
+
+             std::chrono::milliseconds end = std::chrono::duration_cast<std::chrono::milliseconds>(
+                 std::chrono::system_clock::now().time_since_epoch()
+             );
+
+             serv->check_event = { -1, -1, -1, -1 };
+             std::unordered_map<int, std::vector<int>> all_records;
+             while (end - start < (std::chrono::milliseconds)LISTEN_TICK) {
+
+                 for (int i = 0; i < NUM_PLAYERS; i++)
                  {
-                     continue; // continue if we already receive 1 event from this client
+                     if (serv->check_event[i] != -1)
+                     {
+                         continue; // continue if we already receive 1 event from this client
+                     }
+                     all_records[i] = serv->recv_eventRecords(i);
                  }
-                 all_records[i] = serv->recv_eventRecords(i);
+                 end = std::chrono::duration_cast<std::chrono::milliseconds>(
+                     std::chrono::system_clock::now().time_since_epoch()
+                 );
+             }
+
+             serv->updateByEvent(all_records);
+             serv->checkGameEndLogic();
+             for (int j = 0; j < NUM_PLAYERS; j++)
+             {
+                 serv->send_gamedata(j);
              }
              end = std::chrono::duration_cast<std::chrono::milliseconds>(
                  std::chrono::system_clock::now().time_since_epoch()
